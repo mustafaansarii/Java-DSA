999904dbefa2babf6ef24ff6da574a3343a525ca
mustafa ansari <mustafaansari@mail.com>
2024-07-28 23:43:32 +0530

    some changes

--- oops/intro.java
+++ oops/intro.java
@@ -1,5 +1,19 @@
-public class intro {
-    public static void main(String[] args) {
-        System.out.println("hi");
+class Hello {
+    void sayHello() {
+        System.out.println("This is the Hello class.");
+    }
+    void sayHello2() {
+        System.out.println("This is the Hello class.");
     }
 }
+
+
+public class intro {
+    public static void main(String[] args) {
+        System.out.println("This is the intro class.");
+        Hello hello = new Hello();
+        hello.sayHello();
+        hello.sayHello2();
+    }
+}
+
